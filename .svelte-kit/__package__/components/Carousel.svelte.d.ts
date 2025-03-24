bc4b2afe12e93a56ce51bda965f4dad25a1fb77e
ryugenxd <anonimous7770@gmail.com>
2025-03-24 17:03:01 +0800

    refactor: Remove Carousel component and its related type definitions

--- .svelte-kit/__package__/components/Carousel.svelte.d.ts
+++
@@ -1,5 +0,0 @@
-declare const Carousel: import("svelte").Component<{
-    images: any;
-}, {}, "">;
-type Carousel = ReturnType<typeof Carousel>;
-export default Carousel;
--- .svelte-kit/__package__/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Carousel } from './Carousel.svelte';
-// Tambahkan ekspor komponen lain di sini saat Anda membuatnya
